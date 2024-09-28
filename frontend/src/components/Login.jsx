fa195551a1e0af8d95cf8389989143c978b7a91e
02snehil <snehil2024@gmail.com>
2024-09-29 03:08:13 +0530

    add bg in login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {Link, useNavigate} from 'react-router-dom';
+import backgroundImage from '../assets/login.jpg';
+
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: '', password: '' });
@@ -18,6 +20,7 @@ const Login = () => {
     try {
       //const res = await axios.post('http://localhost:5000/api/auth/login', formData);
       const res = await axios.post('https://auth-app-n44y.onrender.com/api/auth/login', formData);
+
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       setMessage(`Welcome, ${res.data.user.username}`);
@@ -30,8 +33,12 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+    <div
+      className="flex flex-col mt-0.5 items-center justify-center w-full min-h-screen  bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
+    >
+    <div div className="bg-white bg-opacity-80 p-8  rounded-lg shadow-md w-full max-w-md min-h-20 text-center">
+      
         <h2 className="text-2xl font-bold mb-6">Login</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
@@ -65,6 +72,7 @@ const Login = () => {
         </form>
       </div>
     </div>
+
   );
 };
 
